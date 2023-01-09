672751808dd2fee05869dec54d8c157764d6baf1
Cheikh <bayecheikhgueye2@gmail.com>
2023-01-09 16:26:24 +0000

    Update

--- app/Models/PostePeage.php
+++ app/Models/PostePeage.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Contenu extends Model
+class PostePeage extends Model
 {
     use HasFactory;
     /**
